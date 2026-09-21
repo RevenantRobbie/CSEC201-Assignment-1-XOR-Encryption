0345f76a7994959ea9df03c16f81068852f20a10
RevenantRobbie <revenantrobbie100@gmail.com>
2026-09-21 01:29:32 -0400

    switched to pwntools for better testing. completed retest of edgecase 1 with same output

---
+++ EdgecaseTester.py
@@ -0,0 +1,30 @@
+from pwn import *
+
+XOR = process("./xor", stdin=process.PTY, stdout=process.PTY)
+
+def testEdgecase1():
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"e")
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"edgecase1.txt")
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"A"*1000)
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"Z")
+
+def testEdgecase2():
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"d")
+    print(XOR.recvuntil(b": "))
+    XOR.sendline(b"edgecase1.txt")
+    print(XOR.recvuntil(b": "))
+    XOR.send(b"Z"*32)
+
+def main():
+    # print(XOR.recvuntil(b": "))
+    # XOR.interactive()
+    testEdgecase1()
+
+if __name__ == "__main__":
+    main()
+
